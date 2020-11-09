f334e3df965a54fdc7ec996ea972fad0b49a808c
wilmerbaby <wilmerbaby@gmail.com>
2020-11-09 16:53:40 -0500

    added config rout and middleware

--- src/containers/folder/SignIn/SignIn.jsx
+++ src/containers/folder/SignIn/SignIn.jsx
@@ -3,11 +3,26 @@ import React, { Component } from 'react';
 class SignIn extends Component {
     render() {
         return (
-            <div>
-                <h1>sing in</h1>
-            </div>
-        );
-    }
-}
+            <>
+        <form>
+  <div className="form-row"/>
+    <div class="form-group col-md-6"/>
+      <label for="inputEmail4">Full Name</label>
+      <input type="email" className="form-control" id="inputEmail4"/>
+      <label for="inputEmail4">Email</label>
+      <input type="email" className="form-control" id="inputEmail4"/>
+      <label for="inputEmail4">password</label>
+      <input type="email" className="form-control" id="inputEmail4"/>
+      <label for="inputEmail4">Email</label>
+      <input type="email" className="form-control" id="inputEmail4"/>
+     
+  
+  
+ 
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+           </>
+        )
+}}
 
 export default SignIn;
