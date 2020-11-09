c5ed0a1de9b25990698e599109fa7e7bb8138e3a
wilmerbaby <wilmerbaby@gmail.com>
2020-11-09 18:01:20 -0500

    fixed robo 3T connection by going to administrive tools and manually starting the connection downtime 1 hr

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
-const mo
+const mongoose = require("mongoose")
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/signIn",{ useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/signIn",{ useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get("/api/config", (req, res) => {
+const connection = mongoose.connection;
+connection.on("connected", () => {
+    console.log("your connectet susccesfully")
+});
+
+connection.on("error", (err) => {
+    console.log("mongoose connection error:", err);
+});
+
+
+app.get("/api/signIn", (req, res) => {
     res.json({
         success: true,
     });
