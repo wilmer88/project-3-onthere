e459f85b52ae7a110f1b8425a743cf72718e4f32
wilmerbaby <wilmerbaby@gmail.com>
2020-11-11 14:41:49 -0500

    adde user sign in mocked api call

--- src/containers/folder/SignIn/SignIn.jsx
+++ src/containers/folder/SignIn/SignIn.jsx
@@ -1,36 +1,63 @@
 // import React, { Component } from 'react';
-import {link} from "react-router-dom"
+// import { link } from "react-router-dom";
+import React, { useState } from "react";
+// function login
 
-
-
+//form signin
 const SignIn = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const submitedForm = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-               <>
-               
-               <div className="container">
-                 <div className="row">
-                   <div className="col-sm-12">
-                   <h1>Please Login</h1>
-                   <form>
-  <div className="form-row"/>
-    <div class="form-group col-md-6"/>
-      <label for="inputEmail4">Full Name</label>
-      <input type="email" className="form-control" id="inputEmail4"/>
-      <label for="inputEmail4">Email</label>
-      <input type="email" className="form-control" id="inputEmail4"/>
-      <label for="inputEmail4">password</label>
-      <input type="email" className="form-control" id="inputEmail4"/>
-      
-  
- 
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
-                   </div>
-                 </div>
-               </div>
-       
-           </>
+      <>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <h1>Please Login</h1>
+              <form onSubmit ={submitedForm}>
+                <div className="form-row" />
+                <div class="form-group col-md-6" />
+
+                <label for="inputEmail4">Full Name</label>
+                <input type="email" className="form-control" id="inputEmail4" />
+
+                <label for="inputEmail4">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="inputEmail4"
+                  value={email}
+                  name="email"
+                  onChange={(e) => {
+                    setemail(e.target.value);
+                  }}
+                />
+
+                <label for="inputEmail4">password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inputpassword"
+                  value={password}
+                  name="password"
+                  onChange={(e) => {
+                    setpassword(e.target.value);
+                  }}
+                />
+
+                <button type="submit" class="btn btn-primary">
+                  Sign in
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
     </div>
   );
 };
