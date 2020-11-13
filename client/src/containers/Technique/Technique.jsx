d30c2fed61743e4af81951fef7627a13ff0e3c07
wilmerbaby <wilmerbaby@gmail.com>
2020-11-13 11:33:58 -0500

    adding front end to the main technique page, making api call to backend

--- client/src/containers/Technique/Technique.jsx
+++ client/src/containers/Technique/Technique.jsx
@@ -1,6 +1,7 @@
 // import React, { Component } from 'react';
 // import React from 'react';
 // import VidCard from "./VidCard/VidCard"
+import {Link} from 'react-router-dom';
 
 const Technique = (props) => {
     return (
@@ -9,7 +10,96 @@ const Technique = (props) => {
           <div class="col">
 
     
-          
+          <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-body">
+      <Link href={"/" }class="card-Link">Card Link</Link>
+        <Link href={"/"} class="card-Link">Another Link</Link>
+      </div>
+    </div>
+
+    <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-body">
+      <Link href={"/" }class="card-Link">Card Link</Link>
+        <Link href={"/"} class="card-Link">Another Link</Link>
+      </div>
+    </div>
+
+
+    <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-body">
+      <Link href={"/" }class="card-Link">Card Link</Link>
+        <Link href={"/"} class="card-Link">Another Link</Link>
+      </div>
+    </div>
+
+
+    <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-body">
+      <Link href={"/" }class="card-Link">Card Link</Link>
+        <Link href={"/"} class="card-Link">Another Link</Link>
+      </div>
+    </div>
+
+    
+    <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-body">
+      <Link href={"/" }class="card-Link">Card Link</Link>
+        <Link href={"/"} class="card-Link">Another Link</Link>
+      </div>
+    </div>
+              
+              
+              
               
               
               
